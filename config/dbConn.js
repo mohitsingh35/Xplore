0064ca170d9a1e33ea9c39b6ce2a468df6e3192c
Rohit Pandey <rd8614196@gmail.com>
2024-04-19 18:49:45 +0530

    Changes made

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,13 +1,33 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const DB_URL =
-  process.env.DATABASE_URI 
-  // || "mongodb://localhost:27017/OAuth_Mongo";
-const dbConn = () => {
-  mongoose.connect(DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-};
+// const DB_URL =
+//   process.env.DATABASE_URI
+//   // || "mongodb://localhost:27017/OAuth_Mongo";
+// const dbConn = () => {
+//   mongoose.connect(DB_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+// };
+
+// module.exports = dbConn;
+
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.DATABASE_URI;
+// || "mongodb://localhost:27017/OAuth_Mongo";
+const client = new MongoClient(uri, {
+  loggerLevel: "debug",
+  sslValidate: true,
+});
+
+async function dbConn() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
 
 module.exports = dbConn;
